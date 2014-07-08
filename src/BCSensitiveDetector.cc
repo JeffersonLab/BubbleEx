d2a4fb2c053cb2f2220671291b8ac25897ad5c06
dgetts <dmgetts@email.wm.edu>
2014-07-08 01:29:38 -0400

    Create BCSensitiveDetector.cc

---
+++ src/BCSensitiveDetector.cc
@@ -0,0 +1,48 @@
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//  
+//  last updated 707/07/2014 11:10:13 PM 
+//  by darren
+// needed for sensitivedetector ability. based off g4 example. adjust if we 
+// end up using it.
+//
+
+#include "BCSensitiveDetector.hh"
+#include "G4HCofThisEvent.hh"
+#include "G4Step.hh"
+#include "G4ThreeVector.hh"
+#include "G4SDManager.hh"
+#include "G4ios.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+BCSensitiveDetector::BCSensitiveDetector(const G4String& name)
+  : G4VSensitiveDetector(name)
+{
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+BCSensitiveDetector::~BCSensitiveDetector()
+{
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void BCSensitiveDetector::Initialize(G4HCofThisEvent*)
+{
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4bool BCSensitiveDetector::ProcessHits(G4Step*, G4TouchableHistory*)
+{
+  G4cout << "Processing hits ...." << G4endl; 
+  return true;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void BCSensitiveDetector::EndOfEvent(G4HCofThisEvent*)
+{
+}
