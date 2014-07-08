22985561b9ba4f47a214eb9a367ea4e556c3ece0
dgetts <dmgetts@email.wm.edu>
2014-07-08 01:26:49 -0400

    Update EventAction.cc

--- src/EventAction.cc
+++ src/EventAction.cc
@@ -1,37 +1,18 @@
-//
-// ********************************************************************
-// * License and Disclaimer                                           *
-// *                                                                  *
-// * The  Geant4 software  is  copyright of the Copyright Holders  of *
-// * the Geant4 Collaboration.  It is provided  under  the terms  and *
-// * conditions of the Geant4 Software License,  included in the file *
-// * LICENSE and available at  http://cern.ch/geant4/license .  These *
-// * include a list of copyright holders.                             *
-// *                                                                  *
-// * Neither the authors of this software system, nor their employing *
-// * institutes,nor the agencies providing financial support for this *
-// * work  make  any representation or  warranty, express or implied, *
-// * regarding  this  software system or assume any liability for its *
-// * use.  Please see the license in the file  LICENSE  and URL above *
-// * for the full disclaimer and the limitation of liability.         *
-// *                                                                  *
-// * This  code  implementation is the result of  the  scientific and *
-// * technical work of the GEANT4 collaboration.                      *
-// * By using,  copying,  modifying or  distributing the software (or *
-// * any work based  on the software)  you  agree  to acknowledge its *
-// * use  in  resulting  scientific  publications,  and indicate your *
-// * acceptance of all terms of the Geant4 Software license.          *
-// ********************************************************************
-//
-//
-// $Id: EventAction.cc 48195 2011-02-02 15:33:39Z jjacquem $
-// GEANT4 tag $Name: geant4-09-04 $
-//
-// 
-
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
-
+//  
+//  last updated 707/07/2014 11:10:13 PM 
+//  by darren
+//
+// this is one of the three sets ( .cc , .hh) of files which we need 
+// the correct names for the detectors to be right inorder for correct 
+// histogram output.
+// nothing is defined here which is linked directely however we have 
+//  secodary or more such as EnergyElectronDump.
+//
+//
+//
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 #include "EventAction.hh"
 
 #include "RunAction.hh"
@@ -80,6 +61,7 @@ void EventAction::EndOfEventAction(const G4Event*)
   histoManager->FillHisto(2, EnergyElectronDump);
   histoManager->FillHisto(3, TrackLRad);
   histoManager->FillHisto(4, TrackLElectronDump);
+// not sure why these are commented out, were there to begin with.
 
   //fill ntuple
   //
